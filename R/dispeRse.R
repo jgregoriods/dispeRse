29fdf7a4bf07bfc6cb93027cb08435ae665ea0f1
jgregoriods <jonas.gregorio@gmail.com>
2022-07-29 13:26:16 +0200

    cleanup, continue doc

--- R/dispeRse.R
+++ R/dispeRse.R
@@ -1,39 +1,56 @@
 #' Simulates first arrival times from one or more origins.
-#' The model uses density-dependent growth and emigration.
-#' The carrying capacity, growth rates and mobility are
-#' allowed to vary with the environment.
-#' 
-#' The simulation starts with n populated cells at
-#' coordinates and start times defined by the parameter
-#' coords and runs for a number of time. Each time step
-#' corresponds to a generation (defined by parameter t).
-#' Growth is applied to every populated cell using a
-#' logistic model and emigration to a neighboring cell is
-#' calculated from an asymptotic threshold model.
 #'
-#' The carrying capacity in the density-dependend growth
-#' and emigration models is determined by an environment
-#' raster, which normally will represent a variable assumed
-#' to affect population density (e.g. net primary
-#' production) scaled to 0-1 range. Migrants are moved to
-#' the cell with the highest environmental value
+#' The model uses density-dependent growth and emigration. The carrying
+#' capacity, growth rates and mobility are allowed to vary with the environment.
+#' 
+#' The simulation starts with n populated cells at coordinates and start times
+#' defined by the parameter coords and runs for a number of time. Each time step
+#' corresponds to a generation (defined by parameter t). Growth is applied to
+#' every populated cell using a logistic model and emigration to a neighboring
+#' cell is calculated from an asymptotic threshold model.
+#'
+#' The carrying capacity (K) in the density-dependend growth and emigration
+#' models is determined by an environment raster, which normally represents a
+#' variable assumed to affect population density (e.g. net primary production)
+#' scaled to 0-1 range.
+#'
+#' While carrying capacity depends linearly on the environment, the dependence
+#' of the growth rate is allowed to be controlled by a power gamma.
+#'
+#' For emigration, a threshold phi is considered, expressed as a fraction of
+#' carrying capacity. Migrants move to the cell with the highest environmental
+#' value in their Moore neighborhood (8 cells).
+#'
+#' Terrain can be represented by a raster specifying barriers (e.g. mountains),
+#' which block movement, and corridors (e.g. rivers), which accelerate movement.
+#' For the latter, an acceleration factor can be specified.
 #' 
 #' @import raster
 #' @import sp
-#' @param environment lorem
-#' @param terrain lorem
-#' @param r lorem
-#' @param phi lorem
-#' @param coords lorem
-#' @param iter lorem
-#' @param t lorem
-#' @param dist lorem
-#' @param accel lorem
-#' @param gamma lorem
-#' @return lorem
+#' @param environment A RasterLayer. The local K and r depend on its values,
+#' which preferably range from 0 to 1 (fraction of maximum K).
+#' @param terrain A RasterLayer. Values must be 0 = no effect, 1 = barrier,
+#' 2 = corridor.
+#' @param r Numeric. The annual growth rate as a fraction.
+#' @param phi Numeric. The fission threshold as a fraction of carrying capacity.
+#' @param coords A DataFrame. Must contain columns x, y, and date with the
+#' coordinates and starting date (yr BP) of each origin. Coordinates must be in
+#' the same system as the environment and terrain layers.
+#' @param num_iter Numeric. Number of iterations (steps) of the model.
+#' @param t Numeric. The duration, in years, of a generation (model time step).
+#' @param dist Numeric. The distance, in km, that migrants move over a
+#' generation.
+#' @param accel Numeric. The factor by which the usual distance is increased
+#' along corridors. E.g. if dist = 50 km and accel = 3, migrants can move up to
+#' 150 km along a corridor.
+#' @param gamma Numeric. A power that controls the shape of the dependency
+#' between r and the environment.
+#' @return A RasterLayer with simulated arrival times.
 #' @export
 #' @useDynLib dispeRse, .registration = TRUE
-run_disp <- function(environment, terrain, r, phi, coords, iter, t, dist, accel, gamma) {
+simulate_dispersal <- function(environment, terrain, coords, num_iter, r=0.025,
+                               phi=0.5, t=25, dist=50, accel=3, gamma=1) {
+    print("Preparing rasters...")
 
     ROBINSON <- CRS("+proj=robin +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
     WGS84 <- CRS("+init=epsg:4326")
@@ -57,19 +74,22 @@ run_disp <- function(environment, terrain, r, phi, coords, iter, t, dist, accel,
     terrain[is.na(values(terrain))] <- -1
     terr_values <- values(terrain)
 
-    grid_coords <- to_grid(coords, environment)
+    grid_coords <- .to_grid(coords, environment)
 
     x <- grid_coords$x
     y <- grid_coords$y
     start <- grid_coords$date
 
+    print("Running model...")
     ret_val <- .C("run_model", nrow=as.integer(NROW), ncol=as.integer(NCOL),
-                population=as.double(population), env=as.double(env_values), arrival=as.integer(arrival),
-                r=as.double(r), phi=as.double(phi),
-                start=as.integer(start), x=as.integer(x), y=as.integer(y), iter=as.integer(iter),
-                num_origins=as.integer(length(x)), t=as.double(t), terrain=as.integer(terr_values),
+                population=as.double(population), env=as.double(env_values),
+                arrival=as.integer(arrival), r=as.double(r), phi=as.double(phi),
+                start=as.integer(start), x=as.integer(x), y=as.integer(y),
+                iter=as.integer(num_iter), num_origins=as.integer(length(x)),
+                t=as.double(t), terrain=as.integer(terr_values),
                 accel=as.integer(accel), gamma=as.double(gamma),
                 PACKAGE="dispeRse")
+    print("Done.")
 
     res <- raster(matrix(ret_val$arrival, nrow=NROW, ncol=NCOL, byrow=TRUE))
     res[values(res) == 0] <- NA
@@ -80,41 +100,11 @@ run_disp <- function(environment, terrain, r, phi, coords, iter, t, dist, accel,
 
 #' Lorem ipsum dolor.
 #'
-#' @import gdistance
 #' @import raster
-#' @param start_coords lorem
-#' @param start_dates lorem
-#' @param environment lorem
-#' @param base_speed lorem
-#' @return lorem
-#' @export
-sim_dispersal <- function(start_coords, start_dates, environment, base_speed=1) {
-    res <- vector(mode="list")
-    for (i in 1:nrow(start_coords)) {
-        tr <- transition(environment, function(x) 1/mean(x), directions=16)
-        tr <- geoCorrection(tr)
-        ac <- accCost(tr, as.numeric(start_coords[i,]))
-        res[[i]] <- start_dates[i] - ((ac / 1000) * base_speed)
-    }
-    if (nrow(start_coords) == 1) {
-        return(res[[1]])
-    } else {
-        s <- stack(res)
-        max_dates <- max(s, na.rm=T)
-        return(max_dates)
-    }
-}
-
-#' Lorem ipsum dolor.
-#'
-#' @import raster
-#' @param coords lorem
-#' @param grid lorem
-#' @return lorem
-#' @export
-to_grid <- function(coords, grid) {
-    #coordinates(coords) <- ~x+y
-    #proj4string(coords) <- proj4string(grid)
+#' @param coords A SpatialPointsDataFrame.
+#' @param grid A RasterLayer.
+#' @return A DataFrame.
+.to_grid <- function(coords, grid) {
     grid_coords <- data.frame(matrix(ncol=3, nrow=0))
     colnames(grid_coords) <- c("x", "y", "date")
     for (i in 1:nrow(coords)) {
@@ -123,27 +113,4 @@ to_grid <- function(coords, grid) {
         grid_coords[i,3] <- coords$date[i]
     }
     return(grid_coords)
-}
-
-#' Lorem ipsum dolor.
-#'
-#' @import raster
-#' @param r lorem
-#' @return lorem
-.scale <- function(r) {
-    return ((r - min(values(r), na.rm=T)) / (max(values(r), na.rm=T) - min(values(r), na.rm=T)))
-}
-
-#' Lorem ipsum dolor.
-#'
-#' @import raster
-#' @param rain lorem
-#' @param temp lorem
-#' @return lorem
-#' @export
-npp <- function(rain, temp) {
-    rain_npp <- 3000 * (1 - exp(-0.000664 * rain))
-    temp_npp <- 3000 / (1 + exp(1.315-0.119 * temp))
-    s <- stack(rain_npp, temp_npp)
-    return(min(s))
 }
--- main.R
+++ main.R
@@ -12,7 +12,7 @@ rnd <- (raster("null_50.tif")-1)
 env <- env+rnd
 
 terr <- raster("layers/sasia_slo_50.tif")
-terr[terr>=0.9] <- 0
+terr[terr>=0.95] <- 0
 terr[terr>0] <- 1
 rv <- raster("rivers_.tif")
 rv[rv>2] <- 2
@@ -22,8 +22,7 @@ terr[terr>=3] <- 1
 #df <- data.frame(x=c(38,72), y=c(36,25), date=c(10500,7700))
 df <- data.frame(x=38, y=36, date=10500)
 
-print("running model")
-res <- run_disp(env, terr, 0.02, 0.5, df, 250, 20, 50, 2, 0)
+res <- simulate_dispersal(env, terr, df, 300)
 
 plot(res, col=viridis(255))
 contour(res, levels=seq(10000, 0, -1000), add=T)
